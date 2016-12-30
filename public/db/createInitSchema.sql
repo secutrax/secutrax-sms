18bf818934b1f82b10b6c7db561c3ba5efe56004
Gopinath Arumugam <goparumu@cisco.com>
2016-12-30 15:22:03 +0530

    changes to DB struct

--- public/db/createInitSchema.sql
+++ public/db/createInitSchema.sql
@@ -1,8 +1,26 @@
--- Table: sectrax.role
+-- Table: sectrax.st_group
 
--- DROP TABLE sectrax.role;
+-- DROP TABLE sectrax.st_group;
 
-CREATE TABLE sectrax.role
+CREATE TABLE sectrax.st_group
+(
+  group_id numeric(10,0) NOT NULL,
+  group_name varchar(255) NOT NULL,
+  CONSTRAINT group_tab_pk PRIMARY KEY (role_id),
+  CONSTRAINT group_tab_un UNIQUE (role_name)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE sectrax.st_group
+  OWNER TO postgres;
+
+
+-- Table: sectrax.st_role
+
+-- DROP TABLE sectrax.st_role;
+
+CREATE TABLE sectrax.st_role
 (
   role_id numeric(10,0) NOT NULL,
   role_name varchar(255) NOT NULL,
@@ -13,33 +31,59 @@ CREATE TABLE sectrax.role
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE sectrax.role
+ALTER TABLE sectrax.st_role
   OWNER TO postgres;
-  
-  
 
--- Table: sectrax.user
 
--- DROP TABLE sectrax.user;
+-- Table: sectrax.st_user
 
-CREATE TABLE sectrax.user
+-- DROP TABLE sectrax.st_user;
+
+CREATE TABLE sectrax.st_user
 (
   user_id numeric(10,0) NOT NULL,
   user_name character varying(255) NOT NULL,
-  mail_id character varying(255) NOT NULL,
-  passwd character varying(255),
-  role_name varchar(255) NOT NULL,
+  user_passwd character varying(255) NOT NULL,
+  user_mail_id character varying(255) NOT NULL,
+  user_mob_no numeric(10,0) NOT NULL,
+  user_otp_option character varying(255) NOT NULL,
+  user_role_name varchar(255) NOT NULL,
+  user_group_name varchar(255) NOT NULL,
+  
   CONSTRAINT user_tab_pk PRIMARY KEY (user_id),
   CONSTRAINT user_tab_un UNIQUE (user_name),
-  CONSTRAINT user_tab_fk FOREIGN KEY (role_name)
-      REFERENCES sectrax.role (role_name) MATCH SIMPLE
+  CONSTRAINT user_tab_fk FOREIGN KEY (user_role_name)
+      REFERENCES sectrax.st_role (role_name) MATCH SIMPLE
+      ON UPDATE RESTRICT ON DELETE CASCADE
+  CONSTRAINT user_grp_fk FOREIGN KEY (user_group_name)
+      REFERENCES sectrax.st_group (group_name) MATCH SIMPLE
       ON UPDATE RESTRICT ON DELETE CASCADE
 )
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE sectrax.user
+ALTER TABLE sectrax.st_user
   OWNER TO postgres;
+ 
+
+-- Table: sectrax.st_institute
+
+-- DROP TABLE sectrax.st_institute;
+
+CREATE TABLE sectrax.st_institute
+(
+  institute_id numeric(10,0) NOT NULL,
+  institute_name varchar(255) NOT NULL,
+  institute_data json,
+  CONSTRAINT institute_tab_pk PRIMARY KEY (institute_id),
+  CONSTRAINT institute_tab_un UNIQUE (institute_name)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE sectrax.st_institute
+  OWNER TO postgres;
+
 
 
 
